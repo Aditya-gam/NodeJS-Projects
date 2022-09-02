53d2bb5795416cf0d2c5869631c5a0f1b80d933a
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2022-09-02 16:33:35 +0530

    Create connect.js

---
+++ Task Manager App/db/connect.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const connectionString =
+  "mongodb+srv://aditya_Gambhir:1F08BsFq01oOLdld@nodeexpressprojects.clhfd8x.mongodb.net/Task-Manager?retryWrites=true&w=majority";
+
+mongoose
+  .connect(connectionString)
+  .then(() => console.log("Connected to the DB..."))
+  .catch((err) => console.log(err));
