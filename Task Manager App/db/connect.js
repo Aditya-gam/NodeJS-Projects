ad333d026d9f19368ea5e2966a1952480ea37d39
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2022-11-25 12:16:27 +0530

    Update connect.js

--- Task Manager App/db/connect.js
+++ Task Manager App/db/connect.js
@@ -3,7 +3,11 @@ const mongoose = require("mongoose");
 const connectionString =
   "mongodb+srv://aditya_Gambhir:1F08BsFq01oOLdld@nodeexpressprojects.clhfd8x.mongodb.net/Task-Manager?retryWrites=true&w=majority";
 
-mongoose
-  .connect(connectionString)
-  .then(() => console.log("Connected to the DB..."))
-  .catch((err) => console.log(err));
+const connectDB = (url) => {
+  return mongoose
+    .connect(url)
+    .then(() => console.log("Connected to the DB..."))
+    .catch((err) => console.log(err));
+};
+
+module.exports = connectDB;
